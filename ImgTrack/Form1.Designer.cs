f1aa1aacb0b9ac1d6a76a21149813f892a0d1bed
surtshow <surtshow@users.noreply.github.com>
2019-09-26 14:53:55 +0200

    Import og Export Kode

--- ImgTrack/Form1.Designer.cs
+++ ImgTrack/Form1.Designer.cs
@@ -32,19 +32,28 @@
             this.pb_left = new System.Windows.Forms.PictureBox();
             this.pb_right = new System.Windows.Forms.PictureBox();
             this.btn_capture = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pb_left)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_right)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
             // 
             this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.splitContainer1.IsSplitterFixed = true;
-            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Location = new System.Drawing.Point(16, 43);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -54,16 +63,18 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.pb_right);
-            this.splitContainer1.Size = new System.Drawing.Size(776, 358);
-            this.splitContainer1.SplitterDistance = 380;
+            this.splitContainer1.Size = new System.Drawing.Size(1035, 441);
+            this.splitContainer1.SplitterDistance = 506;
+            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 0;
             // 
             // pb_left
             // 
             this.pb_left.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pb_left.Location = new System.Drawing.Point(0, 0);
+            this.pb_left.Margin = new System.Windows.Forms.Padding(4);
             this.pb_left.Name = "pb_left";
-            this.pb_left.Size = new System.Drawing.Size(378, 356);
+            this.pb_left.Size = new System.Drawing.Size(504, 439);
             this.pb_left.TabIndex = 0;
             this.pb_left.TabStop = false;
             // 
@@ -71,28 +82,87 @@
             // 
             this.pb_right.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pb_right.Location = new System.Drawing.Point(0, 0);
+            this.pb_right.Margin = new System.Windows.Forms.Padding(4);
             this.pb_right.Name = "pb_right";
-            this.pb_right.Size = new System.Drawing.Size(390, 356);
+            this.pb_right.Size = new System.Drawing.Size(522, 439);
             this.pb_right.TabIndex = 0;
             this.pb_right.TabStop = false;
             // 
             // btn_capture
             // 
-            this.btn_capture.Location = new System.Drawing.Point(13, 376);
+            this.btn_capture.Location = new System.Drawing.Point(16, 492);
+            this.btn_capture.Margin = new System.Windows.Forms.Padding(4);
             this.btn_capture.Name = "btn_capture";
-            this.btn_capture.Size = new System.Drawing.Size(75, 23);
+            this.btn_capture.Size = new System.Drawing.Size(100, 28);
             this.btn_capture.TabIndex = 1;
             this.btn_capture.Text = "&Capture";
             this.btn_capture.UseVisualStyleBackColor = true;
             this.btn_capture.Click += new System.EventHandler(this.Btn_capture_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importSettingsToolStripMenuItem,
+            this.exportSettingsToolStripMenuItem,
+            this.openImageToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // importSettingsToolStripMenuItem
+            // 
+            this.importSettingsToolStripMenuItem.Name = "importSettingsToolStripMenuItem";
+            this.importSettingsToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.importSettingsToolStripMenuItem.Text = "Import Settings";
+            this.importSettingsToolStripMenuItem.Click += new System.EventHandler(this.importSettingsToolStripMenuItem_Click);
+            // 
+            // exportSettingsToolStripMenuItem
+            // 
+            this.exportSettingsToolStripMenuItem.Name = "exportSettingsToolStripMenuItem";
+            this.exportSettingsToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.exportSettingsToolStripMenuItem.Text = "Export Settings";
+            this.exportSettingsToolStripMenuItem.Click += new System.EventHandler(this.exportSettingsToolStripMenuItem_Click);
+            // 
+            // openImageToolStripMenuItem
+            // 
+            this.openImageToolStripMenuItem.Name = "openImageToolStripMenuItem";
+            this.openImageToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.openImageToolStripMenuItem.Text = "Open Image";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 411);
+            this.ClientSize = new System.Drawing.Size(1067, 533);
             this.Controls.Add(this.btn_capture);
             this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -102,7 +172,10 @@
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pb_left)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_right)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -112,6 +185,13 @@
         private System.Windows.Forms.PictureBox pb_left;
         private System.Windows.Forms.PictureBox pb_right;
         private System.Windows.Forms.Button btn_capture;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
     }
 }
 
--- ImgTrack/Form1.cs
+++ ImgTrack/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,53 @@ namespace ImgTrack
         {
             curimg = pb_left.Image;
             pb_right.Image = curimg;
+        }
+
+        private void importSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var open_file = dialog.FileName;
+                using (var reader = new StreamReader(open_file))
+                {
+                    var imported_colors = reader.ReadLine();  
+                    string[] colors = imported_colors.Split(',');
+                    string color_r = colors[0];
+                    string color_b = colors[1];
+                    string color_g = colors[2];
+                    MessageBox.Show("rød: " +color_r+ " blå: " +color_b+ " grøn: " +color_g);
+                }
+                
+                
+            }
+
+        }
+
+        private void exportSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var csv = new StringBuilder();
+            var color_r = 255;
+            var color_b = 255;
+            var color_g = 255;
+            var new_line = string.Format("{0},{1},{2}", color_r, color_b, color_g);
+            csv.Append(new_line);
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "CSV|.csv";
+            dialog.Title = "I reject me humanity, JoJo!";
+
+            StreamWriter writer = null;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                writer = new StreamWriter(dialog.FileName);
+
+                writer.WriteLine(csv);
+
+                writer.Close();
+            }
+
         }
     }
 }
